c7a0aa2f44cf99cd3e1190889e8567bf66b1d79c
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-08 17:13:57 +0900

    [ADD] userInfo

---
+++ Front/src/components/UserInfo/UserInfo.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './UserInfo.scss';
+import pencil from 'assets/images/pencil.svg';
+const UserInfo = () => {
+  return (
+    <>
+      <div className="UserInfo">
+        <div className="UserInfo-Title">
+          안녕하세요, <b>곽준환</b> 님
+        </div>
+        <div className="UserInfo-LabTitle">
+          <img className="UserInfo-LabTitle-Icon" src={pencil} alt="" />
+          <div className="UserInfo-LabTitle-Title">랩실 신청 내역</div>
+        </div>
+        <div className="UserInfo-Lab">
+          <div className="UserInfo-Lab-TableTop">
+            <div className="UserInfo-Lab-TableTop-Item">신청 날짜</div>
+            <div className="UserInfo-Lab-TableTop-Item">랩실 번호</div>
+            <div className="UserInfo-Lab-TableTop-Item">인원</div>
+            <div className="UserInfo-Lab-TableTop-Item">사용 날짜</div>
+            <div className="UserInfo-Lab-TableTop-Item">결과</div>
+          </div>
+          <hr />
+          <div className="UserInfo-Lab-TableList">sadas</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserInfo;
--- Front/src/pages/MainPage.js
+++ Front/src/pages/MainPage.js
@@ -1,10 +1,12 @@
-import React from "react";
-import Main from "components/Main";
+import React from 'react';
+import Main from 'components/Main';
+import UserInfo from 'components/UserInfo/UserInfo';
 
 const MainPage = () => {
   return (
     <div>
       <Main />
+      <UserInfo />
     </div>
   );
 };
