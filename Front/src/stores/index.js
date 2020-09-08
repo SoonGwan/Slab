6186937ba5bc900cc1adedcbe77183f8073f8833
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-09 08:01:53 +0900

    [ADD] userinfostore

--- Front/src/stores/index.js
+++ Front/src/stores/index.js
@@ -1,13 +1,15 @@
-import StartStore from "./Start/StartStore";
-import MainStore from "./Main/MainStore";
-import ShowLabStatusStore from "./ShowLabStatus/ShowLabStatusStore";
-import MajorTipStore from "./MajorTip/MajorTipStore";
+import StartStore from './Start/StartStore';
+import MainStore from './Main/MainStore';
+import ShowLabStatusStore from './ShowLabStatus/ShowLabStatusStore';
+import MajorTipStore from './MajorTip/MajorTipStore';
+import UserInfoStore from './UserInfo/UserInfoStore';
 
 const stores = {
   StartStore: new StartStore(),
   MainStore: new MainStore(),
   ShowLabStatusStore: new ShowLabStatusStore(),
   MajorTipStore: new MajorTipStore(),
+  UserInfoStore: new UserInfoStore(),
 };
 
 export default stores;
