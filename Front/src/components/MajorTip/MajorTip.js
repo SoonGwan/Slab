ca50ef64d1cdacfbba685ba912ba51e3c5894e9b
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-08 19:44:42 +0900

    [ADD] Major Tip Scroll

--- Front/src/components/MajorTip/MajorTip.js
+++ Front/src/components/MajorTip/MajorTip.js
@@ -39,6 +39,62 @@ const MajorTip = () => {
             <div className="MajorTip-Lab-TableList-Name">곽준환</div>
           </div>
           <hr />
+          <div className="MajorTip-Lab-TableList">
+            <div className="MajorTip-Lab-TableList-Item">
+              임베에 떨어질 시 대처 요령
+            </div>
+            <div className="MajorTip-Lab-TableList-Name">곽준환</div>
+          </div>
+          <hr />
+          <div className="MajorTip-Lab-TableList">
+            <div className="MajorTip-Lab-TableList-Item">
+              임베에 떨어질 시 대처 요령
+            </div>
+            <div className="MajorTip-Lab-TableList-Name">곽준환</div>
+          </div>
+          <hr />
+          <div className="MajorTip-Lab-TableList">
+            <div className="MajorTip-Lab-TableList-Item">
+              임베에 떨어질 시 대처 요령
+            </div>
+            <div className="MajorTip-Lab-TableList-Name">곽준환</div>
+          </div>
+          <hr />
+          <div className="MajorTip-Lab-TableList">
+            <div className="MajorTip-Lab-TableList-Item">
+              임베에 떨어질 시 대처 요령
+            </div>
+            <div className="MajorTip-Lab-TableList-Name">곽준환</div>
+          </div>
+          <hr />
+          <div className="MajorTip-Lab-TableList">
+            <div className="MajorTip-Lab-TableList-Item">
+              임베에 떨어질 시 대처 요령
+            </div>
+            <div className="MajorTip-Lab-TableList-Name">곽준환</div>
+          </div>
+          <hr />
+          <div className="MajorTip-Lab-TableList">
+            <div className="MajorTip-Lab-TableList-Item">
+              임베에 떨어질 시 대처 요령
+            </div>
+            <div className="MajorTip-Lab-TableList-Name">곽준환</div>
+          </div>
+          <hr />
+          <div className="MajorTip-Lab-TableList">
+            <div className="MajorTip-Lab-TableList-Item">
+              임베에 떨어질 시 대처 요령
+            </div>
+            <div className="MajorTip-Lab-TableList-Name">곽준환</div>
+          </div>
+          <hr />
+          <div className="MajorTip-Lab-TableList">
+            <div className="MajorTip-Lab-TableList-Item">
+              임베에 떨어질 시 대처 요령
+            </div>
+            <div className="MajorTip-Lab-TableList-Name">곽준환</div>
+          </div>
+          <hr />
         </div>
       </div>
     </>
