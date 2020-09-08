6be4afc9fe101ea6c8d357069b509a635698c9e0
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-08 20:05:43 +0900

    [ADD] Write Tip Button

--- Front/src/components/MajorTip/MajorTip.js
+++ Front/src/components/MajorTip/MajorTip.js
@@ -1,14 +1,37 @@
 import React from "react";
 import "./MajorTip.scss";
 import message from "assets/images/message.png";
+import arrow from "assets/images/arrow.svg";
+
+import { Button, withStyles } from "@material-ui/core";
+
+const ColorButton = withStyles((theme) => ({
+  root: {
+    boxShadow: "none",
+    color: theme.palette.getContrastText("#0077FF"),
+    fontWeight: 300,
+    backgroundColor: "#0077FF",
+    width: "10rem",
+    "&:hover": {
+      boxShadow: "none",
+      backgroundColor: "#0077FF",
+    },
+  },
+}))(Button);
 
 const MajorTip = () => {
   return (
     <>
       <div className="MajorTip">
         <div className="MajorTip-LabTitle">
-          <img className="MajorTip-LabTitle-Icon" src={message} alt="" />
-          <div className="MajorTip-LabTitle-Title">전공 TIP</div>
+          <div className="MajorTip-LabTitle-Area">
+            <img className="MajorTip-LabTitle-Icon" src={message} alt="" />
+            <div className="MajorTip-LabTitle-Title">전공 한줄 TIP</div>
+          </div>
+          <ColorButton variant="contained" onClick={() => {}}>
+            TIP 작성하기
+            <img src={arrow} alt={arrow} />
+          </ColorButton>
         </div>
         <div className="MajorTip-Lab">
           <div className="MajorTip-Lab-TableList">
