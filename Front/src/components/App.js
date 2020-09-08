26673f3f0d30410331a219f1668c4b8c6491d415
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-08 15:15:53 +0900

    [ADD]startpage login

--- Front/src/components/App.js
+++ Front/src/components/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from './Login';
 import Start from './Start';
+import * as Pages from '../pages';
 import StartContainer from 'container/StartContainer/StartContainer';
 const App = () => {
   return (
     <>
-      <StartContainer />
+      <Switch>
+        <Route exact path="/login" render={() => <Pages.StartPage />} />
+      </Switch>
     </>
   );
 };
---
+++ Front/src/pages/StartPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import StartContainer from 'container/StartContainer/StartContainer';
+
+const StartPage = () => {
+  return (
+    <>
+      <div>
+        <StartContainer />
+      </div>
+    </>
+  );
+};
+
+export default StartPage;
