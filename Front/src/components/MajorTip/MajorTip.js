8a1283e5a296d52816312e864dbd4580ad599752
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-08 19:38:41 +0900

    [ADD] Major Tip

---
+++ Front/src/components/MajorTip/MajorTip.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./MajorTip.scss";
+import message from "assets/images/message.png";
+
+const MajorTip = () => {
+  return (
+    <>
+      <div className="MajorTip">
+        <div className="MajorTip-LabTitle">
+          <img className="MajorTip-LabTitle-Icon" src={message} alt="" />
+          <div className="MajorTip-LabTitle-Title">전공 TIP</div>
+        </div>
+        <div className="MajorTip-Lab">
+          <div className="MajorTip-Lab-TableList">
+            <div className="MajorTip-Lab-TableList-Item">
+              임베에 떨어질 시 대처 요령
+            </div>
+            <div className="MajorTip-Lab-TableList-Name">곽준환</div>
+          </div>
+          <hr />
+          <div className="MajorTip-Lab-TableList">
+            <div className="MajorTip-Lab-TableList-Item">
+              임베에 떨어질 시 대처 요령
+            </div>
+            <div className="MajorTip-Lab-TableList-Name">곽준환</div>
+          </div>
+          <hr />
+          <div className="MajorTip-Lab-TableList">
+            <div className="MajorTip-Lab-TableList-Item">
+              임베에 떨어질 시 대처 요령
+            </div>
+            <div className="MajorTip-Lab-TableList-Name">곽준환</div>
+          </div>
+          <hr />
+          <div className="MajorTip-Lab-TableList">
+            <div className="MajorTip-Lab-TableList-Item">
+              임베에 떨어질 시 대처 요령
+            </div>
+            <div className="MajorTip-Lab-TableList-Name">곽준환</div>
+          </div>
+          <hr />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MajorTip;
--- Front/src/pages/MainPage.js
+++ Front/src/pages/MainPage.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Main from 'components/Main';
-import UserInfo from 'components/UserInfo/UserInfo';
+import React from "react";
+import Main from "components/Main";
+import UserInfo from "components/UserInfo/UserInfo";
+import MajorTip from "components/MajorTip";
 
 const MainPage = () => {
   return (
     <div>
       <Main />
       <UserInfo />
+      <MajorTip />
     </div>
   );
 };
