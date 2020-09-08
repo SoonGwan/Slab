38ad450a22d2001c8fce8e41f019c467a715a6a8
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-08 17:39:02 +0900

    [ADD] laboratory entity

--- Back/src/entity/Laboratory.ts
+++ Back/src/entity/Laboratory.ts
@@ -4,6 +4,7 @@ import {
   PrimaryColumn,
   BaseEntity,
   PrimaryGeneratedColumn,
+  CreateDateColumn,
 } from 'typeorm';
 @Entity('Laboratory')
 export default class Laboratory extends BaseEntity {
@@ -21,4 +22,20 @@ export default class Laboratory extends BaseEntity {
     nullable: false,
   })
   labName: string;
+
+  @Column({
+    nullable: false,
+  })
+  date: string;
+
+  @Column({
+    nullable: false,
+  })
+  isHave: string;
+
+  @Column({
+    length: 255,
+    nullable: false,
+  })
+  whoMade: string;
 }
