293f59115200edb2049b710b8173abcedc6ff5d1
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-09-09 09:09:35 +0900

    [ADD] none

--- Front/src/components/common/Modal/Modal.js
+++ Front/src/components/common/Modal/Modal.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Modal.scss';
 import { AiFillCloseSquare } from 'react-icons/ai';
-const Modal = ({ handleClose, width, height, children }) => {
+const Modal = ({ handleClose, width, height, children, none }) => {
   return (
     <>
       <div className="Modal">
         <div className="Modal-PostBackGround" onClick={handleClose}></div>
         <div className="Modal-Wrap" style={{ width: width, height: height }}>
-          <div className="Modal-Cancel">
+          <div className="Modal-Cancel" style={none ? { display: 'none' } : ''}>
             <AiFillCloseSquare
               onClick={() => handleClose()}
               style={{ cursor: 'pointer', width: '20px', height: '20px' }}
